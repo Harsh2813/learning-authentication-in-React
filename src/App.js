a46420b673b5347a344fa445210b93babbb927a4
Harsh2813 <prateektiwari.pk55@gmail.com>
2024-01-13 22:44:59 +0530

    used local storage and now if we login then only url change can work otherwise not don't have to login again after page reload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import React, {useContext} from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+
+  const authCxt = useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        { !authCxt.isLoggedIn && <Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>}
         <Route path='/profile'>
-          <UserProfile />
+          {authCxt.isLoggedIn && <UserProfile />}
+          {!authCxt.isLoggedIn && <Redirect to='/auth'/>}
+        </Route>
+        <Route path='*'>
+          <Redirect to='/'/>
         </Route>
       </Switch>
     </Layout>
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,15 +9,18 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
 
-    const [token, setToken] = useState(null)
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken);//after refreshing page token set so that don't again login
 
     const userIsLoggedIn = !!token;//is reverse the truthy and falsy value like if token is empty string then it returns false if not empty then it returns true;
 
     const userLoginHandler = (token) => {
         setToken(token);// fetch se login krke response data se token liye aur yha ye login fn call kiye fir token set ho gya same ab token me empty string ni h to userIsLoggedIn true ho gya.
+        localStorage.setItem('token', token);
     }
     const userLogoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     }
 
     const contextValue = {
