74d403ad3ea8febf37b21586388033bb5c389458
Harsh2813 <prateektiwari.pk55@gmail.com>
2024-01-13 17:51:34 +0530

    added login also

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -9,7 +9,7 @@ const AuthForm = () => {
   const emailRef = useRef("");
   const passwordRef = useRef("");
 
-  const switchAuthModeHandler = () => {
+  const switchAuthModeHandler = () => { //hm bs form switch kr rhe state change krke input same h bcoz
     setIsLogin((prevState) => !prevState);
   };
 
@@ -21,37 +21,39 @@ const AuthForm = () => {
     //can add input's validation
 
     setIsLoading(true);
+    let url;// same fetch method use krna h different url ke liye isiliye url ke liye taki uske acc. server work karega and hme jo Id token milta h. The token would be used in all the api calls after logging in so we need to store it smartly, so that it could be used
     if (isLogin) {
       //agar isLogin true hua matlab login form wale input submit hue to
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBvM-vQJZQxYvIGdUL9Zgr-NPsIkZuPPXw";
     } else {
       // agr isLogin false hua mtlb signUp wale page me to post kr denge input details of user
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvM-vQJZQxYvIGdUL9Zgr-NPsIkZuPPXw",
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredMail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {'Content-Type': 'application/json'}
-        }
-      ).then((res) =>{
-        setIsLoading(false);
-        if(res.ok){
-          console.log('ok')
-        }else{
-          return res.json().then((data) =>{
-            //show error modal
-            let errorMessage = 'Authentication Failed!';
-            if(data && data.error && data.error.message){
-              errorMessage = data.error.message;
-            }
-            alert(errorMessage);
-          })
-        }
-      })
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvM-vQJZQxYvIGdUL9Zgr-NPsIkZuPPXw";
     }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredMail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json();
+      } else {
+        return res.json().then((data) => {
+          //show error modal
+          let errorMessage = "Authentication Failed!";
+          // if (data && data.error && data.error.message) {
+          //   errorMessage = data.error.message;
+          // }
+          throw new Error(errorMessage);//ye hmne error throw krke niche .catch me handle kr liya
+        });
+      }
+    }).then((data) =>console.log(data)).catch((err) => alert(err.message));
   };
 
   return (
@@ -67,7 +69,11 @@ const AuthForm = () => {
           <input type="password" id="password" ref={passwordRef} required />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button type="submit">{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button type="submit">
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )}
           {isLoading && <p>Sending Request...</p>}
           <button
             type="button"
